6948e325b4a85df0b8e86cad31f8d12d12193bea
Elvis Knapman <elvis@elvisknapman.com>
2021-04-09 17:27:08 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, { useState, useReducer } from 'react';
+
+import TodoInfoBar from './components/TodoInfoBar/TodoInfoBar.component.jsx';
+import TodoItems from './components/TodoItems/TodoItems.component.jsx';
+
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'some todo',
+      urgent: false,
+      completed: false,
+      created: new Date(Date.now()),
+    },
+    {
+      id: 2,
+      title: 'another todo',
+      urgent: false,
+      completed: true,
+      created: new Date(Date.now()),
+    },
+    {
+      id: 3,
+      title: 'yet another todo',
+      urgent: true,
+      completed: false,
+      created: new Date(Date.now()),
+    },
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo App</h1>
+      <TodoInfoBar setTodos={setTodos} />
+      <TodoItems todos={todos} />
     </div>
   );
 }
