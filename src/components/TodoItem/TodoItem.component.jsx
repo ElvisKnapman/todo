39666b3bb7096fd6aa461bd7d617d0e1266e8c5d
Elvis Knapman <elvis@elvisknapman.com>
2021-04-11 17:31:20 -0400

    import context and action types, added functionality for deleting todo when icon clicked

--- src/components/TodoItem/TodoItem.component.jsx
+++ src/components/TodoItem/TodoItem.component.jsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+
+import { StateContext } from '../../StateContext';
+
 import {
   BsExclamationTriangleFill,
   BsFillClockFill,
@@ -6,20 +9,35 @@ import {
   BsCheckCircle,
 } from 'react-icons/bs';
 
+import { ACTION_TYPES } from '../../actionTypes/actionTypes';
+
 import './TodoItem.styles.scss';
 
 const TodoItem = (props) => {
+  const { dispatch } = useContext(StateContext);
   const { todo } = props;
+
+  useEffect(() => {
+    console.log('new todo component mounted');
+  }, []);
   return (
     <div className="todo-item">
       <div className="flex-container">
         <div className="todo-title">{todo.title}</div>
         <div className="todo-status-container">
           <div className="todo-status-icon">
-            <BsCheckCircle className="checkmark-icon" />
+            <BsCheckCircle
+              className="checkmark-icon"
+              onClick={() => console.log('clicked to toggle complete')}
+            />
           </div>
           <div className="todo-status-icon">
-            <BsTrashFill className="trash-icon" />
+            <BsTrashFill
+              className="trash-icon"
+              onClick={() =>
+                dispatch({ type: ACTION_TYPES.DELETE_TODO, payload: todo.id })
+              }
+            />
           </div>
         </div>
 
